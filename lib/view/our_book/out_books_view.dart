e475b952f505d81f82aa529a8d19c58df3c7f655
evrardwems <evrardwemayi8@gmail.com>
2024-01-21 15:59:45 -0800

    some changes are applied

---
+++ lib/view/our_book/out_books_view.dart
@@ -0,0 +1,75 @@
+import 'package:book_grocer/common/color_extenstion.dart';
+import 'package:flutter/material.dart';
+
+class OurBooksView extends StatefulWidget {
+  const OurBooksView({super.key});
+
+  @override
+  State<OurBooksView> createState() => _OurBooksViewState();
+}
+
+class _OurBooksViewState extends State<OurBooksView> {
+
+  List imageArr = ["assets/img/ob1.png","assets/img/ob2.png","assets/img/ob3.png"];
+
+  @override
+  Widget build(BuildContext context) {
+    var media  = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              alignment: Alignment.topCenter,
+              children: [
+
+              Image.asset("assets/img/our_books_top.png", width:  media.width, fit: BoxFit.fitWidth, ),
+             
+
+              AppBar(
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                  leading: IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: const Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+
+                Container(
+                  height: media.width * 0.9 ,
+                 
+                  alignment: Alignment.bottomLeft,
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child:  Row( children: imageArr.map((iName) {
+                      var isFirst= imageArr.first == iName;
+                      var isLast = imageArr.last == iName;
+                    return Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 8),
+                      padding: isFirst ? const EdgeInsets.only(left: 15 ) : ( isLast ? const EdgeInsets.only(right: 15) : null ) ,
+                      child: Image.asset( iName,  height: 120 , fit: BoxFit.fitHeight, ),);
+                  } ).toList() , ),),
+                )
+
+              
+
+            ],),
+
+
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 20),
+              child: Text("""These is are the books we propose""", textAlign: TextAlign.center , style: TextStyle(color: TColor.subTitle, fontSize: 15),),
+            )
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
